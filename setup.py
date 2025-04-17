e44de57018891653e09d755ac2b68268cb295a92
YouTubeMusicAPI <FastMusicYTAPI@gmail.com>
2025-04-17 23:00:33 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,19 @@
+from setuptools import setup, find_packages
 
+setup(
+    name="YouTubeMusic",
+    version="1.2.1",
+    description="Fast YouTube Music Search with DuckDuckGo",
+    author="ABHISHEK THAKUR",
+    author_email="abhishekbanshiwal2005@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+        "aiohttp",
+        "beautifulsoup4",
+    ],
+    entry_points={
+        'console_scripts': [
+            '###',
+        ],
+    },
+)
