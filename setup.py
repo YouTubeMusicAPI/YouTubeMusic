348fcc761bae2d5e8059c810cbb2ede333f54356
YouTubeMusicAPI <FastMusicYTAPI@gmail.com>
2025-04-18 12:38:20 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,21 +2,21 @@ from setuptools import setup, find_packages
 
 setup(
     name="YouTubeMusic",
-    version="1.2.1",
-    description="Fast YouTube Music Search with DuckDuckGo",
+    version="2.1.0",
+    description="A YouTube Music Search Package With Artist, Channel, and Video Details",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
     author="ABHISHEK THAKUR",
     author_email="abhishekbanshiwal2005@gmail.com",
+    url="https://github.com/YouTubeMusicAPI/YouTubeMusic",
     packages=find_packages(),
     install_requires=[
-        "aiohttp",
-        "beautifulsoup4",
+        "httpx",
     ],
-    entry_points={
-        "console_scripts": [
-            "fastyt=cli:main",
-            "youtube=cli:main",
-            "ytmusic=cli:main",
-            "abhimusic=cli:main"
-        ]
-    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',
 )
