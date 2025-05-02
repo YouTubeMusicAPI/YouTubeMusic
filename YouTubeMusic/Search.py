f289c4dcba9afa3f85549cec88f7ff60f9a4cd20
YouTubeMusicAPI <FastMusicYTAPI@gmail.com>
2025-05-03 01:00:56 +0530

    Limit Changes

--- YouTubeMusic/Search.py
+++ YouTubeMusic/Search.py
@@ -3,7 +3,7 @@ import httpx
 import re
 import json
 
-def Search(query: str, max_results: int = 5):
+def Search(query: str, max_results: int = 1):
     url = f"https://www.youtube.com/results?search_query={query.replace(' ', '+')}"
     headers = {
         "User-Agent": "Mozilla/5.0"
