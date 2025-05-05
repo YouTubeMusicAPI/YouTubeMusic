1fc0fc439883aa75e7973a2f17e329c87202a0de
YouTubeMusicAPI <FastMusicYTAPI@gmail.com>
2025-05-05 21:49:45 +0530

    Added Playlist Support By Query Name

--- Search.py
+++ Search.py
@@ -2,7 +2,7 @@ from YouTubeMusic.Search import Search
 
 def main():
     query = input("Enter song name or YouTube URL: ").strip()
-    results = Search(query, limit=1)
+    results = Search(query, limit=3)
 
     if not results:
         print("No results found.")
@@ -10,13 +10,19 @@ def main():
 
     for idx, item in enumerate(results, 1):
         print(f"\nResult {idx}")
+        print("Type      :", item.get("type", "video"))
+
         print("Title     :", item["title"])
-        print("Artist    :", item["artist_name"])
         print("Channel   :", item["channel_name"])
-        print("Duration  :", item["duration"])
-        print("Views     :", item["views"])
         print("Thumbnail :", item["thumbnail"])
         print("URL       :", item["url"])
+
+        if item.get("type") == "playlist":
+            print("Videos    :", item.get("video_count", "Unknown"))
+        else:
+            print("Artist    :", item.get("artist_name", "Unknown"))
+            print("Duration  :", item.get("duration", "Unknown"))
+            print("Views     :", item.get("views", "0"))
 
 if __name__ == "__main__":
     main()
