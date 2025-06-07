8a090907a77d6752d478856a7a2642f4474083fa
YouTubeMusicAPI <FastMusicYTAPI@gmail.com>
2025-06-07 16:19:43 +0530

    New Look And Fastest

--- Test.py
+++ Test.py
@@ -13,7 +13,6 @@ async def main():
     for idx, item in enumerate(results, 1):
         print(f"\nResult {idx}")
         print("Title:", item["title"])
-        print("Artist:", item["artist_name"])
         print("Channel:", item["channel_name"])
         print("Duration:", item["duration"])
         print("Views:", item["views"])
