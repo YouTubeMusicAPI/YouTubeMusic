98ab800b661f96fb44bbc1e68180ed82c52f7ae4
YouTubeMusicAPI <FastMusicYTAPI@gmail.com>
2025-06-07 17:03:30 +0530

    CLI Update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="YouTubeMusic",
-    version="7.0.0",
+    version="7.1.0",
     description="A YouTube Music Search Package With Artist, Channel, and Video Details",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
@@ -15,7 +15,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'ytmusic=YouTubeMusic.cli:main',
+            'ytmusic=YouTubeMusic.cli:cli',
         ],
     },
     classifiers=[
