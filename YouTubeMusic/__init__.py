b458ea9837d0d6729e5e61fd7a77b9003f3db54e
YouTubeMusicAPI <FastMusicYTAPI@gmail.com>
2026-02-26 04:13:36 +0530

    Update __init__.py

--- YouTubeMusic/__init__.py
+++ YouTubeMusic/__init__.py
@@ -1,13 +1,24 @@
 from .YtSearch import Search as YtSearch
 from .Search import Search, close_client
+from .Stream import get_stream
+from .Video_Stream import get_video_audio_urls, stream_merged
 from .Models import format_dur, process_video, extract_artist
 from .Utils import parse_dur, format_ind, format_views
 
-__version__ = "2026.2.25"
+__version__ = "2026.2.26"
 __author__ = "ABHISHEK THAKUR"
 
-try:
-    from .Startup import print_startup_message
-    print_startup_message()
-except Exception:
-    pass
+__all__ = [
+    "YtSearch",
+    "Search",
+    "close_client",
+    "get_stream",
+    "get_video_audio_urls",
+    "stream_merged",
+    "format_dur",
+    "process_video",
+    "extract_artist",
+    "parse_dur",
+    "format_ind",
+    "format_views",
+]
